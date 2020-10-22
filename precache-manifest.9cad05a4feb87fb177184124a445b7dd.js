01c8cb850775d7b1529469e5374800b47becb593
vicky1234-dev <vicky.bhullar1000@gmail.com>
2020-10-22 18:05:44 +0530

    Updates

---
+++ precache-manifest.9cad05a4feb87fb177184124a445b7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a47d013a98ea057e0b5dfd0e15480e",
+    "url": "/buildreact-redux/index.html"
+  },
+  {
+    "revision": "0bdd50d88fe8f3377f2a",
+    "url": "/buildreact-redux/static/css/main.60f3dff1.chunk.css"
+  },
+  {
+    "revision": "f9f730c12580fbaf7ef5",
+    "url": "/buildreact-redux/static/js/2.852e9659.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/buildreact-redux/static/js/2.852e9659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdd50d88fe8f3377f2a",
+    "url": "/buildreact-redux/static/js/main.67f15735.chunk.js"
+  },
+  {
+    "revision": "54e471eb6f63c8d18be3",
+    "url": "/buildreact-redux/static/js/runtime-main.d4f6f705.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buildreact-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
